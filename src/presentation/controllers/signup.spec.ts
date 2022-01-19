c4b04a14aa10388b4672899df6723d73bf5ff4ef
Emerson Ribeiro <emersonrjr03@gmail.com>
2022-01-19 20:46:03 +0000

    test: fixing tests

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -3,13 +3,13 @@ import { SignUpController } from './signup'
 describe('SignUp Controller', () => {
   test('Should return 400 if no name is provided', () => {
     const sut = new SignUpController()
-    const httpRequest = {
+    const httpRequest = Object.create({
       body: {
         email: 'any_email@mail.com',
         password: 'any_password',
         passwordConfirmation: 'any_password'
       }
-    }
+    })
     const httpResponse = sut.handle(httpRequest)
 
     expect(httpResponse.statusCode).toBe(400)
@@ -21,13 +21,13 @@ describe('SignUp Controller', () => {
 describe('SignUp Controller', () => {
   test('Should return 400 if no email is provided', () => {
     const sut = new SignUpController()
-    const httpRequest = {
+    const httpRequest = Object.create({
       body: {
         name: 'any_name',
         password: 'any_password',
         passwordConfirmation: 'any_password'
       }
-    }
+    })
     const httpResponse = sut.handle(httpRequest)
 
     expect(httpResponse.statusCode).toBe(400)
