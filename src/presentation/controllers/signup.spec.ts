6fb77562752f51d310510db029e7117e4b705da4
Emerson Ribeiro <emersonrjr03@gmail.com>
2022-01-19 20:51:07 +0000

    refactor: add customized error for missing params

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,3 +1,4 @@
+import { MissingParamError } from '../errors/missing-param-error'
 import { SignUpController } from './signup'
 
 describe('SignUp Controller', () => {
@@ -14,7 +15,7 @@ describe('SignUp Controller', () => {
 
     expect(httpResponse.statusCode).toBe(400)
 
-    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+    expect(httpResponse.body).toEqual(new MissingParamError('name'))
   })
 })
 
@@ -32,6 +33,6 @@ describe('SignUp Controller', () => {
 
     expect(httpResponse.statusCode).toBe(400)
 
-    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
+    expect(httpResponse.body).toEqual(new MissingParamError('email'))
   })
 })
