d2281d8e46472282cdebe96b1a90a0bdbab0faef
Emerson Ribeiro <emersonrjr03@gmail.com>
2022-01-24 18:56:03 +0000

    refactor: removing duplicated code

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -17,13 +17,14 @@ export class SignUpController implements Controller {
           return badRequest(new MissingParamError(field))
         }
       }
-      const isValid = this.emailValidator.isValid(httpRequest.body.email)
-      if (!isValid) {
-        return badRequest(new InvalidParamError('email'))
+      const { email, password, passwordConfirmation } = httpRequest.body
+      if (password !== passwordConfirmation) {
+        return badRequest(new InvalidParamError('passwordConfirmation'))
       }
 
-      if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) {
-        return badRequest(new InvalidParamError('passwordConfirmation'))
+      const isValid = this.emailValidator.isValid(email)
+      if (!isValid) {
+        return badRequest(new InvalidParamError('email'))
       }
     } catch (error) {
       return serverError()
