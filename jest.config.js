a5a615ca37fe79d59adcc139c39267403b17c581
Emerson Ribeiro <emersonrjr03@gmail.com>
2022-01-27 19:12:19 +0000

    chore: add jest configs and scripts

--- jest.config.js
+++ jest.config.js
@@ -12,9 +12,6 @@ module.exports = {
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
 
-  // Indicates which provider should be used to instrument code for coverage
-  coverageProvider: 'v8',
-
   // The test environment that will be used for testing
   testEnvironment: 'node',
 
