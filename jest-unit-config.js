8ba1ee536aae8799c3a2b80fbae1bff802926f31
Emerson Ribeiro <emersonrjr03@gmail.com>
2022-02-02 22:08:36 +0000

    chore: fixing jest testing typescript issue

--- jest-unit-config.js
+++ jest-unit-config.js
@@ -1,4 +1,4 @@
 // arquivo js que altera a propriedade que define pra quais arquivos olhar durante o test
 const config = require('./jest.config')
-config.testMatch = ['*/*.spec.ts']
-module.export = config
+config.testMatch = ['**/*.spec.ts']
+module.exports = config
