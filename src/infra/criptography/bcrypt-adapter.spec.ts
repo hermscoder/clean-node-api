4b05bf0b9fe24fc07a35830e4293ddadeb038944
Emerson Ribeiro <emersonrjr03@gmail.com>
2022-02-04 18:45:19 +0000

    test: ensure bcryptAdapter trows if bcrypt throws

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -28,4 +28,16 @@ describe('BCrypt Adapter', () => {
 
     expect(hash).toBe('hash')
   })
+
+  test('Should throw if bcrypt throws', async () => {
+    const sut = makeSut()
+    jest.spyOn(bcrypt, 'hash').mockImplementationOnce(
+      () => {
+        throw new Error()
+      }
+    )
+    const promise = sut.encrypt('any_value')
+
+    await expect(promise).rejects.toThrow()
+  })
 })
